1de923e8aee5f59a488bd45e3922e05d0fd55767
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-04-09 12:56:21 +0100

    GetRawBody

--- index.js
+++ index.js
@@ -98,7 +98,28 @@ router
     // Check for Order
     console.log('🎉 We got an order!')
 
+    // We'll compare the hmac to our own hash
+    const hmac = req.get('X-Shopify-Hmac-Sha256')
 
+    // Use raw-body to get the body (buffer)
+    const body = await getRawBody(req)
+
+    // Create a hash using the body and our key
+    const hash = crypto
+      .createHmac('sha256', secretKey)
+      .update(body, 'utf8', 'hex')
+      .digest('base64')
+
+    // Compare our hash to Shopify's hash
+    if (hash === hmac) {
+      // It's a match! All good
+      console.log('It came from Shopify!')
+      res.sendStatus(200)
+    } else {
+      // No match! This request didn't originate from Shopify
+      console.log('Not from Shopify!')
+      res.sendStatus(403)
+    }
 
 });
 
